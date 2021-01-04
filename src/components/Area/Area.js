6e5d4708bc5f9ae63b2964160f7398607e2a4259
Ruslan Valeev <ruvaleev@gmail.com>
2021-01-04 19:29:09 +0700

    Add choose Area event listeners to Areas

--- src/components/Area/Area.js
+++ src/components/Area/Area.js
@@ -1,34 +1,55 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import TodosCard from '../TodosCard';
 
-const Area = ({ area, choosenAreaId, chooseArea }) => {
-  useEffect(() => {
-    const links = document.querySelectorAll(`[data-link-id=${area.id}]`);
-    links.forEach((link) => {
-      link.addEventListener('click', () => {
-        chooseArea(area.id);
-        const renderedArea = document.getElementById(area.id);
-        const areaWidth = renderedArea.offsetWidth;
-        // assuming that width of side menu is 25%
-        const areaScreenWidth = window.screen.width * 0.75;
-        const sideWidth = (areaScreenWidth - areaWidth) / 2;
-        const areasCard = document.getElementById('areas-card');
-        const scrollLeftEdge = renderedArea.offsetLeft - sideWidth;
+function scrollTo(element, scrollLeftEdge) {
+  element.scroll({ left: scrollLeftEdge, behavior: 'smooth' });
+}
 
-        areasCard.scroll({ left: scrollLeftEdge, behavior: 'smooth' });
+function makeAreaChoosen(renderedArea, areasCard, areaScreenWidth, chooseArea, e) {
+  if (!renderedArea.classList.contains('choosen-area')) {
+    e.preventDefault();
+    e.stopPropagation();
+    chooseArea(renderedArea.id);
+
+    const areaWidth = renderedArea.offsetWidth;
+    const sideWidth = (areaScreenWidth - areaWidth) / 2;
+    const scrollLeftEdge = renderedArea.offsetLeft - sideWidth;
+
+    scrollTo(areasCard, scrollLeftEdge);
+  }
+}
+
+class Area extends React.Component {
+  componentDidMount() {
+    const { area, chooseArea } = this.props;
+    const links = document.querySelectorAll(`[data-link-id=${area.id}]`);
+    const areasCard = document.getElementById('areas-card');
+    const renderedArea = document.getElementById(area.id);
+    // assuming that width of side menu is 25%
+    const areaScreenWidth = window.screen.width * 0.75;
+
+    links.forEach((link) => {
+      link.addEventListener('click', (e) => {
+        makeAreaChoosen(renderedArea, areasCard, areaScreenWidth, chooseArea, e);
       });
     });
-  });
+    renderedArea.addEventListener('click', (e) => {
+      makeAreaChoosen(renderedArea, areasCard, areaScreenWidth, chooseArea, e);
+    });
+  }
 
-  return (
-    <div key={area.id} id={area.id} className={`mx-16 min-w-20em flex flex-col items-center relative area ${choosenAreaId === area.id && 'choosen-area'}`}>
-      <div className="mb-4 title text-big font-light">{area.fields.title}</div>
-      <TodosCard areaId={area.id} />
-    </div>
-  );
-};
+  render() {
+    const { area, choosenAreaId } = this.props;
+    return (
+      <div key={area.id} id={area.id} className={`mx-16 min-w-20em flex flex-col items-center relative area ${choosenAreaId === area.id && 'choosen-area'}`}>
+        <div className="mb-4 title text-big font-light">{area.fields.title}</div>
+        <TodosCard areaId={area.id} />
+      </div>
+    );
+  }
+}
 
 Area.propTypes = {
   area: PropTypes.objectOf(
