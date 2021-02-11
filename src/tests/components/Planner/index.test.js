b6f833e07e035f9e0645add92b49f4618a1af503
Ruslan Valeev <ruvaleev@gmail.com>
2021-02-11 18:19:49 +0700

    Add redirection in unauthorized to Planner Page

--- src/tests/components/Planner/index.test.js
+++ src/tests/components/Planner/index.test.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import i18n from 'i18next';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 
 import Planner from '../../../components/Planner';
 import Store from '../../shared/Store';
 import AuthenticationsReducerGenerator from '../../shared/AuthenticationsReducerGenerator';
+import { plannerPath, rootPath } from '../../../helpers/routes';
 
 Element.prototype.scroll = jest.fn();
+
+const history = createMemoryHistory();
 
 function renderWithStore(store) {
   return render(
     <Provider store={store}>
-      <Planner />
+      <Router history={history}>
+        <Planner />
+      </Router>
     </Provider>,
   );
 }
@@ -57,6 +64,23 @@ describe('Planner', () => {
 
     it("doesn't render message about demo mode", () => {
       expect(component.queryByText(i18n.t('demo mode message'))).not.toBeInTheDocument();
+    });
+  });
+
+  describe('when user unauthenticated', () => {
+    beforeEach(() => {
+      store = mockStore(Store({
+        authenticationsReducer: AuthenticationsReducerGenerator({
+          isAuthenticated: false,
+        }),
+      }));
+      history.push(plannerPath());
+      store.dispatch = jest.fn();
+      component = renderWithStore(store);
+    });
+
+    it('redirects to Home Page', () => {
+      expect(history.location.pathname).toBe(rootPath());
     });
   });
 });
