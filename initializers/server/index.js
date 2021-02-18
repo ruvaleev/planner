b63172976db338735561f734c8ae8989ab3e76ae
Ruslan Valeev <ruvaleev@gmail.com>
2021-02-18 18:07:16 +0700

    Add Rollbar integration

--- initializers/server/index.js
+++ initializers/server/index.js
@@ -1,12 +1,18 @@
 import Express from 'express';
+import Rollbar from 'rollbar';
 
 import handleRender from './handleRender';
+
+const rollbar = new Rollbar(process.env.ROLLBAR_SERVER_ITEM_ACCESS_TOKEN);
 
 const app = Express();
 const port = process.env.PORT || 3000;
 
 // Serve static files
 app.use('/assets', Express.static('./dist/assets/'));
+
+// Use the rollbar error handler to send exceptions to your rollbar account
+app.use(rollbar.errorHandler());
 
 // This is fired every time the server side receives a request
 app.use(handleRender);
